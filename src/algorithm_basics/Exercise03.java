b5fd7cf3e1f6ea448e807963f04b818d56ac99e9
Igor Costa <igorcosta.silva10a@gmail.com>
2026-03-30 16:47:14 -0300

    feat: exercicio 03

---
+++ src/algorithm_basics/Exercise03.java
@@ -0,0 +1,22 @@
+package algorithm_basics;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite um valor em segundos: ");
+        int segundosTotais = teclado.nextInt();
+
+        int horas = segundosTotais / 3600;
+        int minutos = (segundosTotais % 3600) / 60;
+        int segundos = segundosTotais % 60;
+
+        System.out.printf("%02d:%02d:%02d", horas, minutos, segundos);
+    }
+}
+
+/*Leia um valor em segundos e mostre esse tempo no formato h:m:s
+(horas, minutos e segundos).*/
